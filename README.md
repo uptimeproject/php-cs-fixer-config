1356a24301a8528ac84de7641617f386f109af92
Jesse Kramer <jkramer@pcextreme.nl>
2020-05-27 08:21:37 +0200

    Add rule implementation

---
+++ README.md
@@ -0,0 +1,53 @@
+<a href="https://uptimeproject.io" target="_blank"><img src="https://uptimeproject.io/img/logo.png" height="50px" /></a>
+
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/uptimeproject/php-cs-fixer-config/CI?style=flat-square)
+![Packagist PHP Version Support](https://img.shields.io/packagist/php-v/uptimeproject/php-cs-fixer-config?style=flat-square)
+![Packagist PHP Version Support](https://img.shields.io/packagist/v/uptimeproject/php-cs-fixer-config?style=flat-square)
+![Packagist Downloads](https://img.shields.io/packagist/dt/uptimeproject/php-cs-fixer-config?style=flat-square)
+
+This is a shared [FriendsOfPHP/php-cs-fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer) configuration used primarily in UptimeProject.io projects.
+But feel free to use it in your projects if you would like. Also, if you want to propose a change, feel free to create a PR 😁
+
+## Installation
+
+```sh
+composer require --dev uptimeproject/php-cs-fixer-config
+```
+
+## Usage
+
+Create a `.php_cs.dist` configuration file in the root of your project.
+
+```php
+<?php
+
+$config = new UptimeProject\PhpCsFixerConfig\Config;
+$config->getFinder()
+    ->in(__DIR__ . "/src")
+    ->in(__DIR__ . "/tests");
+
+return $config;
+```
+
+### Adding/overriding rules
+
+It can be the case that you want to change something in the rules. No problem, you can override or add rules as follows:
+
+```php
+<?php
+
+$config = new UptimeProject\PhpCsFixerConfig\Config([
+    'declare_strict_types' => true,
+]);
+$config->getFinder()
+    ->in(__DIR__ . "/src")
+    ->in(__DIR__ . "/tests");
+
+return $config;
+```
+
+Be aware when you do this that risky fixers are allowed by default!
+
+## License
+
+The MIT License (MIT). Please see [License File](https://github.com/uptimeproject/php-cs-fixer-config/blob/master/LICENSE) for more information.
---
+++ src/Config.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace UptimeProject\PhpCsFixerConfig;
+
+use PhpCsFixer\Config as BaseConfig;
+
+class Config extends BaseConfig
+{
+    /** @var array */
+    private $overrides = [];
+
+    /**
+     * Config constructor.
+     *
+     * @param array $ruleOverrides You can override the standard ruleset.
+     */
+    public function __construct(array $ruleOverrides = [])
+    {
+        parent::__construct('UptimeProject.io shared coding standard');
+        $this->overrides = $ruleOverrides;
+        $this->setRiskyAllowed(true);
+    }
+
+    public function getRules(): array
+    {
+        return array_merge([
+            // Base rule sets
+            '@PSR1' => true,
+            '@PSR2' => true,
+
+            // Additional rules
+            'align_multiline_comment' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'cast_spaces' => ['space' => 'single'],
+            'compact_nullable_typehint' => true,
+            'concat_space' => ['spacing' => 'one'],
+            'declare_equal_normalize' => ['space' => 'single'],
+            'function_typehint_space' => true,
+            'hash_to_slash_comment' => true,
+            'list_syntax' => ['syntax' => 'short'],
+            'lowercase_cast' => true,
+            'method_separation' => true,
+            'new_with_braces' => true,
+            'no_blank_lines_after_class_opening' => true,
+            'no_blank_lines_after_phpdoc' => true,
+            'no_empty_comment' => true,
+            'no_empty_phpdoc' => true,
+            'no_empty_statement' => true,
+            'no_extra_consecutive_blank_lines' => true,
+            'no_leading_import_slash' => true,
+            'no_leading_namespace_whitespace' => true,
+            'no_null_property_initialization' => true,
+            'no_php4_constructor' => true,
+            'no_short_bool_cast' => true,
+            'no_spaces_around_offset' => true,
+            'no_trailing_comma_in_list_call' => true,
+            'no_unneeded_control_parentheses' => true,
+            'no_unneeded_curly_braces' => true,
+            'no_unneeded_final_method' => true,
+            'no_unreachable_default_argument_value' => true,
+            'no_unused_imports' => true,
+            'no_useless_else' => true,
+            'no_useless_return' => true,
+            'no_whitespace_before_comma_in_array' => true,
+            'no_whitespace_in_blank_line' => true,
+            'normalize_index_brace' => true,
+            'not_operator_with_successor_space' => true,
+            'object_operator_without_whitespace' => true,
+            'ordered_class_elements' => true,
+            'ordered_imports' => true,
+            'php_unit_strict' => true,
+            'phpdoc_align' => true,
+            'phpdoc_indent' => true,
+            'phpdoc_order' => true,
+            'phpdoc_scalar' => true,
+            'phpdoc_separation' => true,
+            'phpdoc_single_line_var_spacing' => true,
+            'phpdoc_summary' => true,
+            'phpdoc_types' => true,
+            'phpdoc_var_without_name' => true,
+            'pow_to_exponentiation' => true,
+            'psr4' => true,
+            'return_type_declaration' => ['space_before' => 'none'],
+            'semicolon_after_instruction' => true,
+            'short_scalar_cast' => true,
+            'simplified_null_return' => true,
+            'single_blank_line_before_namespace' => true,
+            'single_quote' => true,
+            'space_after_semicolon' => true,
+            'standardize_not_equals' => true,
+            'ternary_operator_spaces' => true,
+            'trailing_comma_in_multiline_array' => true,
+            'whitespace_after_comma_in_array' => true,
+        ], $this->overrides);
+    }
+}
