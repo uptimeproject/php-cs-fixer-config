f601fec422e7af225cc68d777d76e1996cfb3dc8
Jesse Kramer <jkramer@pcextreme.nl>
2020-05-27 12:29:02 +0200

    Removed simplified null return from ruleset

--- src/Config.php
+++ src/Config.php
@@ -84,7 +84,6 @@ class Config extends BaseConfig
             'return_type_declaration' => ['space_before' => 'none'],
             'semicolon_after_instruction' => true,
             'short_scalar_cast' => true,
-            'simplified_null_return' => true,
             'single_blank_line_before_namespace' => true,
             'single_quote' => true,
             'space_after_semicolon' => true,
